f849ad9c6f36bb881c600c8273deaf0d10f73ff6
ppingkasinan <57752014+ppingkasinan@users.noreply.github.com>
2022-04-18 00:19:37 +0700

    home page

--- src/pages/add.jsx
+++ src/pages/add.jsx
@@ -1,3 +1,8 @@
 export const Add =()=>{
-    return <div>add page</div>
+    return <div className="px-8 pt-14 pb-24">
+        <div className="text-xl text-primary-400 font-bold">Notify For Medicine</div>
+        <div className="text-gray-400 mt-2 text-lg">Do not forget to take your 
+medicine</div>
+        <div className="text-lg text-primary-400 font-bold mt-3">Schedule</div>
+    </div>
 }
