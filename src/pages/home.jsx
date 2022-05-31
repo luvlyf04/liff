8aa0ceba4206b1e1c19a10eebae0847502ae2d44
ppingkasinan <57752014+ppingkasinan@users.noreply.github.com>
2022-05-31 21:07:24 +0700

    สุ่มรูป

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,39 @@
+import { getSchedule } from "../service/google-sheet";
+import React, {useEffect,useState } from "react";
+
 export const Home = () => {
-  const PharmacyCard=({pharmacyName,takeTime, meal, tablet, pharmacyImg}) => {
-    return <div className="w-full bg-gray-200 rounded flex px-6 py-4 gap-x-12 my-4">
-    <img src={pharmacyImg} className="h-20 w-20"></img>
-    <div className="flex-col text-primary-400">
-      <p className="text-lg font-bold">{pharmacyName}</p>
-      <p>{takeTime} {meal}</p>
-      <p>จำนวน {tablet} เม็ด</p>
-    </div>
-  </div>
-  }
+  const [mySchedule, setMySchedule]=useState([])
+  useEffect(() => {
+    getSchedule().then((response) => {
+      setMySchedule(response.data)
+    });
+  }, []);
+  const pharmacyImages = [
+    "/assets/images/med1.png",
+    "/assets/images/med2.png",
+    "/assets/images/med3.png",
+    "/assets/images/med4.png",
+  ];
+  const PharmacyCard = ({
+    pharmacyName,
+    takeTime,
+    meal,
+    tablet,
+    pharmacyImg,
+  }) => {
+    return (
+      <div className="w-full bg-gray-200 rounded flex px-6 py-4 gap-x-12 my-4">
+        <img src={pharmacyImg} className="h-20 w-20"></img>
+        <div className="flex-col text-primary-400">
+          <p className="text-lg font-bold">{pharmacyName}</p>
+          <p>
+            {takeTime} {meal}
+          </p>
+          <p>จำนวน {tablet} เม็ด</p>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="px-8 pt-14 pb-24">
       <div className="text-xl text-primary-400 font-bold">
@@ -18,10 +43,19 @@ export const Home = () => {
         Do not forget to take your medicine
       </div>
       <div className="text-lg text-primary-400 font-bold mt-3">Schedule</div>
-      <PharmacyCard pharmacyName="ยาแก้ปวด" takeTime="ก่อนอาหาร" meal="กลางวัน" tablet="1" pharmacyImg={"/assets/images/med.png"}></PharmacyCard>
-      <PharmacyCard></PharmacyCard>
-      <PharmacyCard></PharmacyCard>
-      <PharmacyCard></PharmacyCard>
+      {mySchedule.map(() => {
+        return (
+          <PharmacyCard
+            pharmacyName="ยาแก้ปวด"
+            takeTime="ก่อนอาหาร"
+            meal="กลางวัน"
+            tablet="1"
+            pharmacyImg={
+              pharmacyImages[Math.floor(Math.random() * pharmacyImages.length)]
+            }
+          ></PharmacyCard>
+        );
+      })}
     </div>
   );
 };
--- src/service/google-sheet.js
+++ src/service/google-sheet.js
@@ -3,3 +3,6 @@ import axios from "axios"
 export const insert = (data)=> {
     return axios.post("https://sheet.best/api/sheets/01ae957e-d32a-44f1-8ad0-0156cde7dc3f",data)
 }
+export const getSchedule = ()=> {
+    return axios.get("https://sheet.best/api/sheets/01ae957e-d32a-44f1-8ad0-0156cde7dc3f")
+}
